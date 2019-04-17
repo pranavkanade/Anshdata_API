61b342591d03edcee756b7700b02dc9c2b40ace7
pskanade <k.pranav.apk@gmail.com>
2019-04-18 03:35:44 +0530

    Added more fields to User model and separated userManager

--- core/models.py
+++ core/models.py
@@ -1,50 +1,17 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.utils import timezone
-from django.core.mail import send_mail
 from django.utils.translation import gettext_lazy as _
+from django.core.mail import send_mail
+from django.utils import timezone
 
-
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def _create_user(self, username, email, password, **extra_fields):
-        """
-        Create and save a user with the given username, email, and password.
-        """
-        if not username:
-            raise ValueError('The given username must be set')
-        if not email:
-            raise ValueError('The given email address must be set')
-        email = self.normalize_email(email)
-        username = self.model.normalize_username(username)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, email, password, **extra_fields)
-
-    def create_superuser(self, username, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(username, email, password, **extra_fields)
+from core.managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """
-
+    @NOTE: Is the derived from the `django.contrib.auth.models.AbstractUser`
     """
     username_validator = UnicodeUsernameValidator()
 
@@ -58,7 +25,6 @@ class User(AbstractBaseUser, PermissionsMixin):
             'unique': _("A user with that username already exists."),
         },
     )
-
     email = models.EmailField(_('user email'),
                               blank=False,
                               unique=True,
@@ -83,6 +49,8 @@ class User(AbstractBaseUser, PermissionsMixin):
                                       editable=False,
                                       help_text="Stores the last login time and date of the user")
 
+    objects = UserManager()
+
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
@@ -90,9 +58,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
-        abstract = True
-
-    objects = UserManager()
+        abstract = False
 
     def clean(self):
         super().clean()
@@ -106,6 +72,9 @@ class User(AbstractBaseUser, PermissionsMixin):
 class UserProfile(models.Model):
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    profile_dp = models.ImageField(_('profile picture'), blank=True)
+    bio = models.TextField(_('bio'), max_length=255, blank=True)
+
     company_name = models.CharField(_("Company Name"),
                                     max_length=255,
                                     blank=True,
@@ -119,7 +88,6 @@ class UserProfile(models.Model):
     last_updated = models.DateTimeField(_("Info updated on"),
                                         null=True,
                                         blank=True,
-                                        editable=False,
                                         help_text="This field reflects the date and time"
                                                   " when the user information was last updated")
 
@@ -133,107 +101,3 @@ class UserProfile(models.Model):
     def get_short_name(self):
         """Return the short name for the user."""
         return self.first_name
-
-
-class Course(models.Model):
-    title = models.CharField(_("Title"),
-                             max_length=255,
-                             blank=False,
-                             null=False)
-    # description = models.TextField(_("Course's Description"),
-    #                                blank=True,
-    #                                null=True,
-    #                                default="Test Course")
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               blank=False,
-                               null=False,
-                               related_name="courses",
-                               help_text="Refers to the creator account of the course")
-    # estimate_time_required = models.IntegerField(_("Estimate Time"),
-    #                                              default=0,
-    #                                              blank=True,
-    #                                              null=False,
-    #                                              help_text="Approx. time required to complete the course")
-    students = models.ManyToManyField(User,
-                                      blank=True,
-                                      related_name="enrolled_in")
-
-
-class Unit(models.Model):
-    title = models.CharField(_("Title"),
-                             max_length=255,
-                             blank=False,
-                             null=False)
-    # description = models.TextField(_("Unit's Description"),
-    #                                blank=True,
-    #                                null=True,
-    #                                default="Test Unit")
-    course = models.ForeignKey(Course,
-                               on_delete=models.CASCADE,
-                               blank=False,
-                               null=False,
-                               related_name="units",
-                               help_text="Refers to the course this unit belongs to")
-
-
-class Lesson(models.Model):
-    title = models.CharField(_("Title"),
-                             max_length=255,
-                             blank=False,
-                             null=False)
-    # link_to_lecture = models.URLField(_("Video URL"),
-    #                                   blank=True,
-    #                                   null=True,
-    #                                   default="https://www.youtube.com/watch?v=6MSksJhPcVA")
-    unit = models.ForeignKey(Unit,
-                             on_delete=models.CASCADE,
-                             blank=False,
-                             null=False,
-                             editable=True,
-                             related_name="lessons")
-
-
-class Assignment(models.Model):
-    title = models.CharField(_("Title"),
-                             max_length=255,
-                             blank=False,
-                             null=False)
-    # description = models.TextField(_("Course's Description"),
-    #                                blank=True,
-    #                                null=True,
-    #                                default="Test Course")
-
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               blank=False,
-                               null=False,
-                               related_name="assignments_contributed",
-                               help_text="Refers to the creator account of the course")
-
-    # Assignments may not be related to any one single lesson or unit.
-    # In that case lesson FK will be left blank.
-    lesson = models.ForeignKey(Lesson,
-                               on_delete=models.CASCADE,
-                               blank=True,
-                               null=True,
-                               editable=True,
-                               related_name="assignments")
-    unit = models.ForeignKey(Unit,
-                             on_delete=models.CASCADE,
-                             blank=True,
-                             null=True,
-                             editable=True,
-                             related_name="assignments")
-    course = models.ForeignKey(Course,
-                               on_delete=models.CASCADE,
-                               blank=False,
-                               null=False,
-                               editable=True,
-                               related_name="assignments")
-
-    # estimate_time_required = models.IntegerField(_("Estimate Time"),
-    #                                              default=0,
-    #                                              blank=True,
-    #                                              null=False,
-    #                                              help_text="Approx. time required to complete the assignment in hrs")
