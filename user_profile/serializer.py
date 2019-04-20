6cdf578e96d0ad6de7958961d153926cc1a53777
pskanade <k.pranav.apk@gmail.com>
2019-04-20 14:53:25 +0530

    Can create simple user

--- user_profile/serializer.py
+++ user_profile/serializer.py
@@ -1,8 +1,5 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth import get_user_model
-from core.models import Producer, Consumer
-
-from course.serializers.id_serializers import CourseIdSerializer
 
 
 class UserSerializer(ModelSerializer):
@@ -11,56 +8,16 @@ class UserSerializer(ModelSerializer):
     """
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'password', 'is_producer')
+        fields = ('id', 'username', 'email', 'password')
         read_only_fields = ('id',)
         extra_kwargs = {
             'password': {'write_only': True}    # one can add min_length here
         }
 
-    # following function needs to be explicitly defined
+    # following function needs to be explicitly defined as we have defined manager explicitly
     def create(self, validated_data):
+        print("[create] : UserSerializer")
         """
         Create a new user with encrypted password and return the user
         """
         return get_user_model().objects.create_user(**validated_data)
-
-
-class UserDetailSerializer(ModelSerializer):
-    """
-    Serialize User model
-    """
-    class Meta:
-        model = get_user_model()
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_producer', 'date_joined')
-        read_only_fields = ('id',)
-
-
-class ProducerSerializer(ModelSerializer):
-    """
-    Serialize a producer object
-    """
-    class Meta:
-        model = Producer
-        fields = ('user', 'company_name')
-
-
-class ProducerDetailedSerializer(ProducerSerializer):
-    user = UserDetailSerializer(many=False, read_only=True)
-
-
-class ConsumerSerializer(ModelSerializer):
-    class Meta:
-        model = Consumer
-        fields = ('user', 'date_of_birth')
-
-
-class ConsumerDetailedSerializer(ConsumerSerializer):
-    user = UserDetailSerializer(many=False, read_only=True)
-
-
-class UserCoursesEnrolledSerializer(ModelSerializer):
-    courses = CourseIdSerializer(many=True, source="enrolled_in")
-
-    class Meta:
-        model = get_user_model()
-        fields = ('id', 'courses')
--- user_profile/views.py
+++ user_profile/views.py
@@ -4,10 +4,7 @@ from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from user_profile.serializer import \
-    UserSerializer, \
-    ProducerDetailedSerializer, \
-    ConsumerDetailedSerializer, \
-    UserCoursesEnrolledSerializer
+    UserSerializer
 
 
 class UserListView(ListAPIView):
@@ -22,13 +19,7 @@ class UserCreateView(CreateAPIView):
 
     def get_serializer_class(self, *args, **kwargs):
         print("[get_serializer_class] : UserCreateView")
-        is_producer = False
-        if 'is_producer' in self.request.data.keys():
-            is_producer = self.request.data['is_producer']
-        if is_producer:
-            return ProducerDetailedSerializer
-        else:
-            return ConsumerDetailedSerializer
+        return UserSerializer
 
     def post(self, request, *args, **kwargs):
         print("[post] : UserCreateView")
@@ -36,14 +27,7 @@ class UserCreateView(CreateAPIView):
 
     def perform_create(self, serializer):
         print("[perform_create] : UserCreateView")
-        is_producer = False
-        if 'is_producer' in self.request.data.keys():
-            is_producer = self.request.data['is_producer']
-        generic_user = get_user_model().objects.create_user(username=self.request.data['username'],
-                                                            email=self.request.data['email'],
-                                                            password=self.request.data["password"],
-                                                            is_producer=is_producer)
-        serializer.save(user=generic_user)
+        serializer.save()
 
 
 class UserGetView(ListAPIView):
@@ -57,7 +41,7 @@ class UserGetView(ListAPIView):
 
 class ListUserEnrolledCoursesView(ListAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = UserCoursesEnrolledSerializer
+    # serializer_class = UserCoursesEnrolledSerializer
     queryset = get_user_model().objects.all()
 
     def get_queryset(self):
