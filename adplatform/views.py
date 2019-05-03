d864c867f63a3ebe2f82fb65b3f93449457697cd
pskanade <k.pranav.apk@gmail.com>
2019-05-03 23:40:38 +0530

    Will allow any one to get the list of categories

--- adplatform/views.py
+++ adplatform/views.py
@@ -1,11 +1,11 @@
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from adplatform.serializers.category import CategorySerializer
 from core.models import Category
 
 
 class CategoryListCreateView(ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
